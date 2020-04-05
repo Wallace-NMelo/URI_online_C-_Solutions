65b1e92008b2ceaff50d64cc75f645a775a97ffe
Wallace-NMelo <wallace.nm@outlook.com>
2020-04-04 21:14:49 -0300

    1001 Extremely Basic

---
+++ 1001.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int a,b,x;
+    
+    cin>>a;
+    
+    cin>>b;
+    
+    x=a+b;
+    
+    cout<<"X = "<< x<< endl;
+    
+ 
+ 
+    return 0;
+}
