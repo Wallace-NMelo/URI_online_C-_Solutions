9719e1e2a02c9329dded2df3b797552034d50c83
Wallace-NMelo <wallace.nm@outlook.com>
2020-04-04 21:08:16 -0300

    1002 Area of a Circle

---
+++ 1002_circle.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+
+    double R,n,A;
+    
+    n=3.14159;
+    
+    cin>>R;
+    
+    A=n*R*R;
+    
+    cout<<"A="<<fixed<<setprecision(4)<<A<<endl;
+ 
+    return 0;
+}
