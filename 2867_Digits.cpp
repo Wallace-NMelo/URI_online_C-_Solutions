99690101601e220cd644a82ef89292cf9e59aa15
Wallace-NMelo <wallace.nm@outlook.com>
2020-04-04 23:35:29 -0300

    2867 - Digits

---
+++ 2867_Digits.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+
+int main(){
+    int C;
+    cin >> C;
+    
+    for (int i=0;i<C;i++){
+        
+    int N,M,out;
+    
+    double log;
+    
+    cin >> N >> M;
+    
+    log = M*log10(N);
+    
+    cout<<int(log)+1<<endl;
+    
+    
+}
+    return 0;
+    
+}
