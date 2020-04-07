9bd78a8dc66d4839180dedf73ab1880295ec9633
Wallace-NMelo <wallace.nm@outlook.com>
2020-04-07 16:31:27 -0300

    2862_Insect

---
+++ 2862.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+int main() {
+    int n,level;
+    cin>>n;
+    
+    for(int i=0;i<n;i++){
+    cin>>level;
+
+        if(level<=8000){
+        cout<<"Inseto!"<<endl;
+        }else{
+        cout<<"Mais de 8000!"<<endl;
+        }
+}
+    
+
+    return 0;
+
+
+}
